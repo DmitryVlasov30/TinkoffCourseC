f5043062ca03575390e54fbba5cd3b4fd798e269
dima_vls <vds788502@gmail.com>
2024-12-02 01:23:41 +0400

    исправил опечатку

--- dynamic_programming_2/O-Sum-Maximum_Increasing_Subsequence.cpp
+++ dynamic_programming_2/O-Sum-Maximum_Increasing_Subsequence.cpp
@@ -21,4 +21,53 @@
 #define len(x) x.size()
 #define all(x) x.begin(), x.end()
 #define ld long double
-#define f
+#define fir first
+#define sec second
+#define emp emplace_back
+
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e7;
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n; cin >> n;
+    vector<int> arr;
+    for(int i = 0; i < n; ++i){
+        int el; cin >> el;
+        arr.emp(el);
+    }
+
+    vector<int> dp(n, 0), prev(n, -1);
+    for(int i = 0; i < n; ++i){
+        dp[i] = arr[i];
+    }
+
+    int sum_m = 0, idx_m = 0;
+
+    for (int i = 0; i < n; ++i){
+        for (int j = 0; j < i; ++j){
+            if (arr[i] > arr[j] && dp[j] + arr[i] > dp[i]){
+                dp[i] = dp[j] + arr[i];
+                prev[i] = j;
+            }
+        }
+        if (dp[i] > sum_m){
+            sum_m = dp[i]; idx_m = i;
+        }
+    }
+
+    vector<int> res;
+    int i = idx_m;
+    while(i != -1){
+        res.emp(arr[i]);
+        i = prev[i];
+
+    }
+    reverse(all(res));
+    for (int i = 0; i < len(res); ++i){
+        cout << res[i] << " ";
+    }
+}
