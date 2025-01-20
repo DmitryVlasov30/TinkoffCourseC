7629e121753bbc2906a0bec1466101a5057b0de6
dima <vds788502@gmail.com>
2025-01-21 00:29:46 +0400

    A-BFS на очереди

---
+++ Graphs, BFS + Dijkstra/A-BFS is next.cpp
@@ -0,0 +1,72 @@
+//#define _GLIBCXX_DEBUG
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+#include <stdio.h>
+#include <strstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <deque>
+
+#define int long long
+#define len(x) x.size()
+#define all(x) x.begin(), x.end()
+#define ld long double
+#define fir first
+#define sec second
+#define emp emplace_back
+
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e18;
+const int maxn = 1e5;
+
+vector<int> graf[maxn + 1];
+vector<int> c(maxn + 1);
+vector<int> bfs(int start, int n){
+    vector<int> dist(n+1, inf);
+    dist[start] = 0;
+    queue<int> q;
+    q.push(start);
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (auto v : graf[u]){
+            if (dist[v] == inf){
+                q.push(v);
+                dist[v] = dist[u] + 1;
+            }
+        }
+    }
+    return dist;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m, s; cin >> n >> m >> s;
+    for (int i = 0; i < m; ++i){
+        int u, v; cin >> u >> v;
+        graf[u].emp(v); graf[v].emp(u);
+    }
+    
+    vector<int> ans = bfs(s, n);
+    for (int i = 1; i < len(ans); ++i){
+        if (ans[i] == inf){
+            cout << -1 << " ";
+        } else {
+            cout << ans[i] << " ";
+        }
+    }
+}
