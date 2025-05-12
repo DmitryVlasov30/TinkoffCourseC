c39cec5005af93c136530ce3117aae372c110f0c
dima <vds788502@gmail.com>
2025-05-13 02:06:40 +0400

    G-Игра с бинарной строкой

--- Interactives/G-Game with binary string.cpp
+++ Interactives/G-Game with binary string.cpp
@@ -1,3 +1,52 @@
-//
-// Created by diwex on 13.05.2025.
-//
+//#define _GLIBCXX_DEBUG
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+#include <stdio.h>
+#include <strstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <deque>
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#define int long long
+#define len(x) x.size()
+#define all(x) x.begin(), x.end()
+#define ld long double
+#define fir first
+#define sec second
+#define emp emplace_back
+#define ins insert
+
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e18;
+const int maxn = 2e6;
+const int logn = 20;
+const int p = 31;
+
+signed main() {
+    int n; cin >> n;
+    string s; cin >> s;
+    cout << "Alice" << endl;
+    while (true){
+        cout << 1 << endl;
+        int ans; cin >> ans;
+        if (ans == 0){
+            return 0;
+        }
+    }
+}
