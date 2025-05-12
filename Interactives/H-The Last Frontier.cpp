60742b2d9618956b7ac7d6ae7d2150ae04b8100e
dima <vds788502@gmail.com>
2025-05-13 02:06:07 +0400

    H-Последний рубеж

---
+++ Interactives/H-The Last Frontier.cpp
@@ -0,0 +1,56 @@
+//#define _GLIBCXX_DEBUG
+#include <iostream>
+#include <map>
+#include <string>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <limits>
+#include <cmath>
+#include <stdio.h>
+#include <strstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <random>
+#include <deque>
+
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+
+#define int long long
+#define len(x) x.size()
+#define all(x) x.begin(), x.end()
+#define ld long double
+#define fir first
+#define sec second
+#define emp emplace_back
+#define ins insert
+
+using namespace std;
+
+const int mod = 1e9 + 7;
+const int inf = 1e18;
+const int maxn = 2e6;
+const int logn = 20;
+const int p = 31;
+
+signed main() {
+    int n; cin >> n;
+    int left = 1, right = n;
+    while (right - left > 1){
+        int mid = (left + right) / 2;
+        cout << "? " << mid << endl;
+        int ans; cin >> ans;
+        if (ans){
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+
+    cout << "! " << left << endl;
+}
